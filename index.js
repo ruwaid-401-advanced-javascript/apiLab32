f09da53d6ebf8b20bbafd65cbc288a5516de9e11
rowaidsayyed <rowaidsayyed@gmail.com>
2020-07-06 21:31:20 +0300

    server

---
+++ index.js
@@ -0,0 +1,30 @@
+  
+'use strict';
+
+/**
+ * will start the server
+ * @module index
+ */
+
+
+// 3rd party lib
+require('dotenv').config();
+const mongoose = require('mongoose');
+// internal modules
+const server = require('./src/server.js');
+
+const MONGODB_URI = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 3000;
+
+
+server.start(PORT);
+
+
+const mongooseOptions = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+};
+
+mongoose.connect(MONGODB_URI, mongooseOptions);
---
+++ src/server.js
@@ -0,0 +1,53 @@
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+
+const error404 = require('./middleware/404');
+const error500 = require('./middleware/500');
+const schema = require('../model.js');
+
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => res.status(200).send('hiii  401'));
+
+
+
+app.route('/todo')
+  .get(async (req, res) => {
+    let record = await schema.find({});
+    res.json(record);
+  })
+  .post(async (req, res) => {
+    try {
+      let record = new schema(req.body);
+      let save = await record.save();
+      res.json(save);
+    } catch (e) {
+      console.log(e.message);
+    }
+  })
+  .put(async (req, res) => {
+    let _id = req.body;
+    let record = await schema.findByIdAndUpdate(_id, req.body, {
+      new: true,
+    });
+    res.json(record);
+  })
+  .delete(async (req, res) => {
+    let _id = req.body;
+    let record = await schema.findByIdAndDelete(req.body._id);
+    res.json(record);
+  });
+
+
+
+app.use(error404);
+app.use(error500);
+
+module.exports = {
+  server: app,
+  start:  (portNumber) => app.listen(portNumber, () => console.log(`Listnening to PORT ${portNumber}`)),
+};
