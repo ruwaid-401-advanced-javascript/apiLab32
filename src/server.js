9b0993e91bf45b00a445a3fd4fab0393fea63d84
rowaidsayyed <rowaidsayyed@gmail.com>
2020-07-08 13:06:44 +0300

    add signin/signup/users rout

--- src/server.js
+++ src/server.js
@@ -2,14 +2,15 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3000;
 
 const error404 = require('./middleware/404');
 const error500 = require('./middleware/500');
 const schema = require('../model.js');
-
-
+const usersSchema = require('../userModel');
+const basicAuth = require('./middleware/basic')
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -45,19 +46,62 @@ app.route('/todo')
   });
 
 
-  app.put('/todo/:id',async (req, res) => {
-    let _id = req.params.id;
-    let record = await schema.findByIdAndUpdate(_id, req.body, {
-      new: true,
-    });
-    res.json(record);
-  })
-  app.delete('/todo/:id',async (req, res) => {
-    let _id = req.params.id;
-    let record = await schema.findByIdAndDelete({_id});
-    res.json(record);
+app.put('/todo/:id', async (req, res) => {
+  let _id = req.params.id;
+  let record = await schema.findByIdAndUpdate(_id, req.body, {
+    new: true,
   });
+  res.json(record);
+})
+app.delete('/todo/:id', async (req, res) => {
+  let _id = req.params.id;
+  let record = await schema.findByIdAndDelete({ _id });
+  res.json(record);
+});
 
+
+
+
+
+
+app.post('/signup', async (req, res, next) => {
+  try {
+
+    let users = new usersSchema(req.body);
+    let result = await users.save();
+    let token = usersSchema.generateToken(result);
+    res.status(200).send({ result, token });
+  } catch (e) {
+    next('error username is duplicated');
+  }
+});
+
+app.post('/signin', basicAuth, (req, res) => {
+
+  // let token = req.token;
+  let token = usersSchema.generateToken(req.data);
+  res.cookie('token', token);
+
+  let roles = {
+    user: ['read'],
+    writers: ['read', 'create'],
+    editors: ['read', 'update', 'create'],
+    admin: ['read', 'update', 'create', 'delete'],
+  };
+  let note1 = 'THIS is a note for you guys ---> users roles have these capabilities';
+  let note2 = `user: ['read']`
+  let note3 = `writers: ['read', 'create']`
+  let note4 = `editors: ['read', 'update', 'create']`
+  let note5 = `admin: ['read', 'update', 'create', 'delete']`
+
+  res.status(200).json({ note1, note2, note3, note4, note5, 'token': token, 'user': req.data, capabilities: roles[req.data.role] });
+});
+
+app.get('/users', async (req, res) => {
+
+  let users = await usersSchema.findAll();
+  res.status(200).json({ users });
+});
 
 
 app.use(error404);
@@ -65,5 +109,5 @@ app.use(error500);
 
 module.exports = {
   server: app,
-  start:  (portNumber) => app.listen(portNumber, () => console.log(`Listnening to PORT ${portNumber}`)),
+  start: (portNumber) => app.listen(portNumber, () => console.log(`Listnening to PORT ${portNumber}`)),
 };
---
+++ userModel.js
@@ -0,0 +1,97 @@
+'use strict';
+
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+
+const Users = mongoose.Schema({
+  username: { type: String, required: true},
+  password: { type: String, required: true },
+  email:{ type: String, required: true },
+  role: { type: String, default: 'user' },
+});
+
+// Users.pre('save', async function () {
+//   console.log('oooooo');
+  
+//   this.password = await bcrypt.hash(this.password, 2);
+//   console.log(this.password,'pppppppppp');
+//   return;
+// });
+
+
+/**
+ * @method athenticateRole
+ * @param {string} user
+ * @param {string} capability
+ */
+
+Users.statics.athenticateRole = function (user, capability) {
+  let roles = {
+    user: ['read'],
+    writers: ['read', 'create'],
+    editors: ['read', 'update', 'create'],
+    admin: ['read', 'update', 'create', 'delete'],
+  };
+  return !!roles[user.capabilities].includes(capability);
+};
+
+/**
+ * @method authenticate
+ * @param {string} username
+ * @param {string} password
+ */
+Users.statics.authenticate = function (username, pass) {
+  return this.find({ username })
+    .then(async (user) => {
+      return bcrypt.compare(pass, user[0].password) ? user[0] : null;
+    });
+};
+
+/**
+ * @method generateToken
+ * @param {object} user
+ * @returns {string} token
+ */
+Users.statics.generateToken = function (user) {
+  let expire = process.env.EXPIRE;
+  return jwt.sign({
+    id: user.id,
+    capabilities: user.role,
+  }, process.env.SECRET, { expiresIn: expire });
+};
+
+/**
+ * @method findAll
+ * find all saved users in DB
+ */
+Users.statics.findAll = function () {
+  return this.find({});
+};
+
+/**
+ * @method findOneByUser
+ * @param {string} username
+ */
+Users.statics.findOneByUser = function (username) {
+  return this.find({ username });
+};
+
+/**
+ * @method verifyToken
+ * @param {string} token
+ */
+Users.statics.verifyToken = async function (token) {
+  return jwt.verify(token, process.env.SECRET, function (err, decoded) {
+    if (err) {
+      return Promise.reject(err);
+    }
+    if (decoded.id && decoded.capabilities) {
+      return Promise.resolve(decoded);
+    } else {
+      return Promise.reject();
+    }
+  });
+};
+
+module.exports = mongoose.model('Users', Users);
