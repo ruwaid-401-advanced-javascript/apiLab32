54797f7141fc20ca7d4cdd3c18bbfbce1cf0e02b
rowaidsayyed <rowaidsayyed@gmail.com>
2020-07-08 20:43:58 +0300

    fix basic

--- src/middleware/basic.js
+++ src/middleware/basic.js
@@ -19,11 +19,17 @@ const users = require('../../userModel');
 const base64 = require('base-64');
 
 module.exports = (req, res, next) => {
+  console.log('heeaaaader',req.headers);
+  console.log('basic',basic);
+  console.log('[user, pass]',user, pass);
+  
   let basic = req.headers.authorization.split(' ').pop();
   let [user, pass] = base64.decode(basic).split(':');  
 
     users.authenticate(user, pass)
       .then(validUser => {
+        console.log('validUser',validUser);
+        
         req.token = users.generateToken(validUser);
         req.data = validUser;
         next();
