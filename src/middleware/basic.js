f5c25cd13c8621b232fa46b2da94d258f29674b9
rowaidsayyed <rowaidsayyed@gmail.com>
2020-07-08 20:39:37 +0300

    fix basic

--- src/middleware/basic.js
+++ src/middleware/basic.js
@@ -21,12 +21,6 @@ const base64 = require('base-64');
 module.exports = (req, res, next) => {
   let basic = req.headers.authorization.split(' ').pop();
   let [user, pass] = base64.decode(basic).split(':');  
-  if (process.env.AUTH !== 'ON') {
-    users.find({ username:user }).then((data) => {     
-      req.data = data;
-      return next();
-    });
-  } else {
 
     users.authenticate(user, pass)
       .then(validUser => {
@@ -36,5 +30,5 @@ module.exports = (req, res, next) => {
         return;
       })
       .catch(err => next('Invalid Login!!'));
-  }
+
 };
